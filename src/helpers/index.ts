037e9a8b60fa8f75351d4f09b778bf19bcedb37e
Marlon Santos <marlon.santos@service.nsw.gov.au>
2024-05-01 16:19:51 +1000

    Update isTextInput export

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,7 @@
 export { default as isPressable } from './isPressable';
 export { default as isAdjustable } from './isAdjustable';
 export { default as isText } from './isText';
+export { default as isTextInput } from './isTextInput';
 export { default as isHidden } from './isHidden';
 export { default as isCheckbox } from './isCheckbox';
 
