1f8e4c36e39ee810f1feec98d5452f8a4daded5a
Marlon Santos <marlon.santos@service.nsw.gov.au>
2024-09-18 16:32:40 +1000

    Ignore the  rule check if child Text has parent

--- src/rules/text-role-required/index.ts
+++ src/rules/text-role-required/index.ts
@@ -1,12 +1,21 @@
 import type { Rule } from '../../types';
-import { isText } from '../../helpers';
+import { isPressable, isText } from '../../helpers';
+import type { ReactTestInstance } from 'react-test-renderer';
 
 const allowedRoles = ['header', 'link', 'text', 'none', 'button'];
 const allowedRolesMessage = allowedRoles.join(' or ');
 
+const hasPressableParent = (node: ReactTestInstance): boolean => {
+  if (isPressable(node.type)) return true;
+
+  if (node.parent) return hasPressableParent(node.parent);
+
+  return false;
+};
+
 const rule: Rule = {
   id: 'text-role-required',
-  matcher: (node) => isText(node.type),
+  matcher: (node) => isText(node.type) && !hasPressableParent(node),
   assertion: (node) => allowedRoles.includes(node.props.accessibilityRole),
   help: {
     problem:
